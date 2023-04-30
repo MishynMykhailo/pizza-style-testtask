6a5cb4e1360ba9ec748a3b8f9ff41de1b00fa0a1
MishynMykhailo <misham18092001@gmail.com>
2023-04-30 23:46:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/">
+        <BrowserRouter basename="/pizza-style-testtask/">
           <App />
         </BrowserRouter>
       </PersistGate>
